f5fd25da2b759a22e238b793d40bacc3a750b6ad
Victor Semenchuk <semenchuk.vitk@gmail.com>
2018-08-24 16:30:38 +0300

    Made MainViewController as initial

---
+++ Giphy/Controllers/MainViewController/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Giphy
+//
+//  Created by Viktar Semianchuk on 8/24/18.
+//  Copyright © 2018 Viktar Semianchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Giphy/Controllers/MainViewController/MainViewController.m
@@ -0,0 +1,21 @@
+//
+//  MainViewController.m
+//  Giphy
+//
+//  Created by Viktar Semianchuk on 8/24/18.
+//  Copyright © 2018 Viktar Semianchuk. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end

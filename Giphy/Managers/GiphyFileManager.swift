7db5204202b8b8cbb8693ec163ff2aace1877aff
Victor Semenchuk <semenchuk.vitk@gmail.com>
2018-08-26 16:46:27 +0300

    Added ability to create and remove directories

--- Giphy/Managers/GiphyFileManager.swift
+++ Giphy/Managers/GiphyFileManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum FileType: String {
-    case gif = ".gif"
+    case gif = "gif"
 }
 
 enum DirectoryType {
@@ -18,26 +18,55 @@ enum DirectoryType {
 
 class GiphyFileManager {
     
+    private let appDirectory = "Giphy"
+    private let fileManager: FileManager!
+    
+    init() {
+        self.fileManager = FileManager.default
+    }
+    
     func dataFromFileWithName(_ fileName: String, withType type: FileType, from directory: DirectoryType) -> Data? {
-        let path = "/\(fileName).\(type)"
+        let path = "\(fileName).\(type)"
         return dataFromFileWithPath(path: path, from: directory)
     }
     
     func writeFileWithName(_ fileName: String, data: Data, withType type: FileType, to directory: DirectoryType) {
-        let fileManager = FileManager.default
-        let path = "/\(fileName).\(type)"
+        let path = "\(fileName).\(type)"
         let rootDirectory = rootDirectoryForDirectoryType(directory: directory)
         if let rootDirectory = rootDirectory {
-            let fullPath = "\(rootDirectory)\(path)"
+            _ = createAppDirectoryIfNeeded(at: rootDirectory)
+            let fullPath = "\(rootDirectory)/\(appDirectory)/\(path)"
             fileManager.createFile(atPath: fullPath, contents: data, attributes: nil)
+        }
+    }
+    
+    func createAppDirectoryIfNeeded(at rootDirectory: String) -> Bool {
+        let directoryPath = "\(rootDirectory)/\(appDirectory)"
+        let url = URL(fileURLWithPath: directoryPath)
+        do {
+            try fileManager.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            return true
+        } catch {
+            print("Error. Can't create directory")
+            return false
+        }
+    }
+    
+    func removeAppDirectory(from rootDirectory: String) -> Bool {
+        let directoryPath = "\(rootDirectory)/\(appDirectory)"
+        do {
+            try fileManager.removeItem(atPath: directoryPath)
+            return true
+        } catch {
+            print("Error. Can't remove directory")
+            return false
         }
     }
     
     private func dataFromFileWithPath(path: String, from directory: DirectoryType) -> Data? {
         let rootDirectory = rootDirectoryForDirectoryType(directory: directory)
         if let rootDirectory = rootDirectory {
-            let fullPath = "\(rootDirectory)\(path)"
-            print(fullPath)
+            let fullPath = "\(rootDirectory)/\(appDirectory)/\(path)"
             let url = URL(fileURLWithPath: fullPath)
             return try? Data(contentsOf: url)
         } else {

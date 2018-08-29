ce5c4b4acbb88bb02361706271e8ea1a7e43983f
Victor Semenchuk <semenchuk.vitk@gmail.com>
2018-08-29 13:47:00 +0300

    Add some UI details to DetailsViewController

--- Giphy/Controllers/DetailsViewController/DetailsViewController.m
+++ Giphy/Controllers/DetailsViewController/DetailsViewController.m
@@ -8,6 +8,7 @@
 
 #import "DetailsViewController.h"
 #import "DetailsViewController+DetailsViewPresenterDelegate.h"
+#import "UIColor+ThemeColors.h"
 
 @interface DetailsViewController ()
 
@@ -16,10 +17,14 @@
 @property (weak, nonatomic) IBOutlet UILabel *publishedLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *trandingView;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageViewHeightConstraint;
+@property (weak, nonatomic) IBOutlet UIView *imageContainerView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UIButton *shareButton;
 
 @property (nonatomic) DetailsViewPresenter *presenter;
 
 - (void)setupData;
+- (void)enableControls;
 
 @end
 
@@ -34,21 +39,35 @@
 - (void)setupData {
     self.title = @"GIF";
     
+    self.imageContainerView.backgroundColor = [UIColor randomThemeColor];
+    [self.activityIndicator startAnimating];
+    
     CGFloat tempHeight = [self.giphyItem.image.preview.height floatValue];
     CGFloat tempWidth = [self.giphyItem.image.preview.width floatValue];
     self.imageViewHeightConstraint.constant = tempHeight * self.imageView.bounds.size.width / tempWidth;
     if ([self.presenter checkIfItemExists:self.giphyItem]) {
         [self.presenter setSavingStatus:YES];
         self.imageView.image = [self.presenter getExistingImage:self.giphyItem];
+        [self.activityIndicator stopAnimating];
+        [self enableControls];
     } else {
         [self.presenter setSavingStatus:NO];
         [self.presenter fetchOriginalImageForGiphyItem:self.giphyItem completion:^(UIImage * _Nullable image) {
+            [self.activityIndicator stopAnimating];
             self.imageView.image = image;
+            [self enableControls];
         }];
     }
     self.titleLabel.text = self.giphyItem.title;
     self.publishedLabel.text = [@"Published on " stringByAppendingString:[self.presenter convertDateWithInputDate:self.giphyItem.importDatetime]];
     self.trandingView.hidden = [self.giphyItem.trendingDatetime isEqualToString:@"0000-00-00 00:00:00"];
+}
+
+- (void)enableControls {
+    self.saveButton.enabled = YES;
+    self.shareButton.enabled = YES;
+    self.stopPlayButton.enabled = YES;
+    [self.stopPlayButton setImage:[UIImage imageNamed:@"StopIcon"] forState:UIControlStateNormal];
 }
 
 - (IBAction)share:(id)sender {
@@ -67,7 +86,5 @@
     }
     [self.presenter toggleSavingStatus];
 }
-
-
 
 @end
